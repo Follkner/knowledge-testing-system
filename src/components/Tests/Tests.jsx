a90d9bbf4cbbc6d55210cf36d5e56e1eb22080d1
Follkner <follkner1@gmail.com>
2019-04-16 23:11:12 +0300

    add pagination

--- src/components/Tests/Tests.jsx
+++ src/components/Tests/Tests.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import DescTest from '../Test/DescTest.jsx';
 
+const TESTS_PER_PAGE = 3;
+
 class Tests extends Component {
 
 	constructor(props) {
@@ -8,17 +10,42 @@ class Tests extends Component {
 
 		this.state = {
 			data: null,
+			numberOfPages: 0,
+			currentPage: 0,
 		};
+
+		this.displayPages = this.displayPages.bind(this);
+		this.clickOnPage = this.clickOnPage.bind(this);
 	}
 
 	componentDidMount() {
 		fetch("http://localhost:3001/data")
 			.then((res) => res.json())
-			.then((data) => this.setState({ data }));
+			.then((data) => this.setState({ data, numberOfPages: Math.ceil(data.length/TESTS_PER_PAGE)}))
+	}
+
+	clickOnPage(e) {
+		this.setState({currentPage: +e.target.innerHTML-1})
+	}
+
+	displayPages() {
+		const pages = []
+		for(let i = 0; i < this.state.numberOfPages; i++){
+			pages.push(
+				<div onClick = {this.clickOnPage} key = {i} className = "page">
+					{i+1}
+				</div>
+			)
+		}
+		return pages;
 	}
 
 	render() {
-		const arr = this.state.data? this.state.data.map((item, index) => {
+		const displayDescTests = (arr) => {
+			return arr.slice(this.state.currentPage*TESTS_PER_PAGE, (this.state.currentPage+1)*TESTS_PER_PAGE);
+		}
+
+		const arr = this.state.data? displayDescTests(this.state.data).map((item, index) => {
 			return (
 				<DescTest key = {item.id} title = {item.title} tests = {item.tests} id = {item.id}/>
 			)
@@ -28,6 +55,9 @@ class Tests extends Component {
 			<div className = "tests">
 				<h1>Component 'Tests'</h1>
 				{arr}
+				<div className = "pages">
+					{this.displayPages()}
+				</div>
 			</div>
 		);
 	}
