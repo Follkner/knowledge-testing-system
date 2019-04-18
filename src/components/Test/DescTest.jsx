5ac9c38088826925fad9d5aaa79012774b5eb16b
Follkner <follkner1@gmail.com>
2019-04-18 14:03:05 +0300

    add ability to take a test

--- src/components/Test/DescTest.jsx
+++ src/components/Test/DescTest.jsx
@@ -30,8 +30,6 @@ class DescTest extends Component {
 		);
 	}
 
-
-
 }
 
 const mapDispatchToProps = (dispatch) => { 
--- src/components/Test/FullTest.jsx
+++ src/components/Test/FullTest.jsx
@@ -6,13 +6,19 @@ class FullTest extends Component {
 	constructor(props) {
 		super(props);
 		this.createTest = this.createTest.bind(this);
+		this.startTest = this.startTest.bind(this);
+		this.finishTest = this.finishTest.bind(this);
+
+		this.state = {
+			isStart: false,
+			idInterval: null,
+		}
 	}
 
 	createTest(){
-		let arr = [];
-		arr = this.props.tests.map((item, index) => {
+		const arr = this.props.tests.map((item, index) => {
 			return (
-				<div key = {"" + this.props.id + index}>
+				<div className = "test" key = {"" + this.props.id + index}>
 					<h3>{item.question}</h3>
 					{(item.correctAnswers.length == 1)? 
 						item.answers.map((item, i) => {
@@ -42,15 +48,87 @@ class FullTest extends Component {
 		return arr;
 	}
 
-	render() {
-		return(
-			
-			<div className = "test">
-				<h2>{this.props.title}</h2>
-				{this.createTest()}
+	startTest() {
+		this.setState({isStart: true});
+
+		let time = 0;
+
+		let interval = setInterval(() => {
+
+			time++;
+
+			let hours = Math.floor(time / 3600);
+			let minutes = Math.floor((time % (60 * 60)) / (60));
+			let seconds = Math.floor(time % 60);
+
+			document.getElementById("timer").innerHTML = `Time: ${hours} : ${minutes} : ${seconds}`;
+
+		}, 1000);
+
+		this.setState({idInterval: interval});
+	}
+
+	finishTest() {
+		const tests = document.getElementsByClassName("test");
+
+		const answers = [];
+
+		for(let i = 0; i < tests.length; i++){
+			const inputs = Array.from(document.getElementsByTagName("input")).filter((item) => {
+				return item.parentNode.parentNode == tests[i];
+			});
+
+			const buf = []
+			inputs.forEach((item) => {
+				if(item.checked) buf.push(item.nextSibling.innerHTML);
+			})
+
+			answers.push(buf);
+		}
 		
-			</div>
+		clearInterval(this.state.idInterval);
+		
+		fetch(`http://localhost:3001/data/${this.props.id}`)
+			.then((res) => res.json())
+			.then((data) => {
+				const correctAnswers = data.tests.map((item) => {
+					return item.correctAnswers;
+				})
+
+				let score = 0;
+
+				for(let i = 0; i < answers.length; i++){
+					if(answers[i].length === correctAnswers[i].length){
+						let flag = true;
+						for(let j = 0; j < answers[i].length; j++){
+							if(!(answers[i][j] == correctAnswers[i][j])) flag = false;
+						}
+						if(flag) score++;
+					}
+				}
+
+				document.getElementById("results").innerHTML = `Your score: ${score} / ${answers.length}`;
+
+				this.setState({
+					isStart: false,
+					idInterval: null,
+				});
+
+			})
+	}
+
+	render() {
+
+		return(	
+			<div className = "test-full">
+				<h2>{this.props.title}</h2>
+				<div id = "timer"></div>
+				<div id = "results"></div>
+				{this.state.isStart? this.createTest(): null}
+				{this.state.isStart? <button onClick = {this.finishTest}>Finish test</button>
+					: <button onClick = {this.startTest}>Start test</button>}
 			
+			</div>		
 		);
 	}
 
